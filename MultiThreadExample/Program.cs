de47224bb9cf436cadefda631b32662f490d754d
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-10-19 15:46:40 +0200

    Add project files.

---
+++ MultiThreadExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadExample
+{
+    class Program
+    {
+        static void mythread()
+        {
+            Console.WriteLine("In progress thread is: {0}",
+                Thread.CurrentThread.Name);
+
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Completed threas is: {0}",
+                Thread.CurrentThread.Name);
+        }
+
+        static void Main(string[] args)
+        {
+            Thread thr = new Thread(mythread);
+
+            thr.Name = "MyThread";
+            thr.Start();
+
+            thr.IsBackground = false;
+
+            Console.WriteLine("Main Thread Ends!!");
+        }
+    }
+}
